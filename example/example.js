f35879bab9b8c6630e8cce4664c4ba00239c1fae
nir <getnirm@gmail.com>
2022-01-18 11:34:40 +0200

    remove chalk dev dependency

--- example/example.js
+++ example/example.js
@@ -2,13 +2,19 @@
 
 const fs = require('fs')
 const {convertXML} = require('../src/xmlToJson')
-const chalk = require('chalk')
 
 const xmlToConvert = fs.readFileSync(__dirname + '/example.xml', {encoding: 'UTF8'})
 
-console.log(chalk.blue('------ XML ------'))
+const ANSI_RESET = '\u001b[0m'
+const ANSI_BLUE = '\u001b[34m'
+const ANSI_YELLOW = '\u001b[33m'
+
+const blueText = text => `${ANSI_BLUE}${text}${ANSI_RESET}`
+const yellowText = text => `${ANSI_YELLOW}${text}${ANSI_RESET}`
+
+console.log(blueText('------ XML ------'))
 console.log(xmlToConvert)
 console.log('\n\n\n')
 
-console.log(chalk.green('------ JSON ------'))
+console.log(yellowText('------ JSON ------'))
 console.log(JSON.stringify(convertXML(xmlToConvert), null, 4))
